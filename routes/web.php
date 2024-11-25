552fea901cdc0b9ccca5b8a5d234a91244babd1f
Ali zaib <alizaib12220@gmail.com>
2024-11-25 11:35:46 +0500

    Integrate Frontend layout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,39 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+})->name('testimonial');
+
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+
+Route::get('/product-details', function () {
+    return view('product-detail');
+})->name('product.details');
